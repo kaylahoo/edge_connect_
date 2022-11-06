dbacd4a91679f528024b1c087b97c8b24593ddd2
kaylahoo <790970276@qq.com>
2022-11-06 22:04:13 +0800

    wd

--- src/edge_connect.py
+++ src/edge_connect.py
@@ -332,8 +332,7 @@ class EdgeConnect():
             #     outputs_merged = (outputs * masks) + (images * (1 - masks))
 
             output = self.postprocess(outputs_merged)[0]
-            #output_masks = self.postprocess(images * masks)[0]
-            output_masks = images * masks
+            output_masks = self.postprocess(images * masks)[0]
             path = os.path.join(self.results_path, name)
             mask_path = os.path.join(self.results_path, '_mask_').join(name)
             print(index, name)
