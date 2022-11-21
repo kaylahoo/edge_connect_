d656e36fe957d09e772f59de28a3909038a4cf39
kaylahoo <790970276@qq.com>
2022-11-22 00:34:45 +0800

    wd

--- src/depconv2d.py
+++ src/depconv2d.py
@@ -57,6 +57,9 @@ class DepConvBNActiv(nn.Module):
                                                   groups=out_channels)
 
         else:
+            self.large_res = None
+            self.small_res = None
+            self.decrease_channels = None
             self.Dconv = Depthwise_separable_conv(in_channels, out_channels, kernel_size=3, stride=1, padding=1,
                                                   groups=groups)
 
@@ -77,20 +80,18 @@ class DepConvBNActiv(nn.Module):
             return nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1)
 
     def forward(self, images, masks):
+        if self.small_res is not None:
 
-        images_l, masks_l = self.large_res(images, masks)  # 31,1,15
+            images_l, masks_l = self.large_res(images, masks)  # 31,1,15
 
-        images_s = self.small_res(images)  # 3,1,1
-        masks_s = self.small_res(masks)  # 3,1,1
+            images_s = self.small_res(images)  # 3,1,1
+            masks_s = self.small_res(masks)  # 3,1,1
 
-        images = torch.concat([images, images_l, images_s], dim=1)  #
-        masks = torch.concat([masks, masks_l, masks_s], dim=1)   #
+            images = torch.concat([images, images_l, images_s], dim=1)  #
+            masks = torch.concat([masks, masks_l, masks_s], dim=1)   #
 
-        images = self.decrease_channels(images)
-        masks = self.decrease_channels(masks)
-
-        print(images.shape)
-        print(masks.shape)
+            images = self.decrease_channels(images)
+            masks = self.decrease_channels(masks)
 
         images, masks = self.Dconv(images, masks)
 
